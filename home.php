538259b69907221838e2dbb26ba2e8bacbd53b06
ADC4869 <thanhdattran123789@gmail.com>
2024-10-04 16:35:06 +0700

    DAT

--- home.php
+++ home.php
@@ -28,7 +28,6 @@ $student_id = isset($_GET['student_id']) ? $_GET['student_id'] : $user_id;
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -47,7 +46,6 @@ $student_id = isset($_GET['student_id']) ? $_GET['student_id'] : $user_id;
     <link rel="stylesheet" href="./css/footer.css">
     <link rel="stylesheet" href="./css/giaodien.css">
 </head>
-
 <body style="height: 145vh;">
     <header>
         <div class="header">
