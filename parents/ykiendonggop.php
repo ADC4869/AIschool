1ed28706748b7c18c3fcd66c3744f92082736665
Tuvantu <tuvantu251202@gmail.com>
2024-10-04 11:53:29 +0700

    tu115304102024

--- parents/ykiendonggop.php
+++ parents/ykiendonggop.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="../fontawesome-free-6.6.0-web/js/fontawesome.min.js">
     <!-- Css -->
     <link rel="stylesheet" href="../css/global.css">
-    <link rel="stylesheet" href="css/ykiendonggop.css">
+    <link rel="stylesheet" href="css/ykien.css">
 </head>
 
 <body>
@@ -36,14 +36,71 @@
         <div class="main_tab">
             <div class="button">
                 <button type="button" class="received">Đã tiếp nhận</button>
-                <button type="button" class="add">Thêm ý kiến</button>
+                <button type="button" class="add" onclick="openModal()">Thêm ý kiến</button>
             </div>
         </div>
 
+        <div class="main_content">
+            <div class="opinion">
+                <div class="opinion_item">
+                    <a href="">
+                        <div class="opinion_photo">
+                            <img src="../img/hs.jpg" alt="">
+                        </div>
 
-    <div class="main_content">
-        .
-    </div>
+                        <div class="opinion_info">
+                            <span class="title">Góp ý về vấn đề Họp phụ huynh</span>
+                            <span class="content">Kính gửi ban giám hiệu nhà trường, tôi tên ABCD ...</span>
+                        </div>
+
+                        <div class="opinion_icon">
+                            <i data-feather="chevron-right" style="color: #5B6998"></i>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="opinion_item">
+                    <a href="">
+                        <div class="opinion_photo">
+                            <img src="../img/hs.jpg" alt="">
+                        </div>
+
+                        <div class="opinion_info">
+                            <span class="title">Góp ý về vấn đề phiếu liên lạc mỗi tháng</span>
+                            <span class="content">Kính gửi ban giám hiệu nhà trường, tôi tên ABCD ...</span>
+                        </div>
+
+                        <div class="opinion_icon">
+                            <i data-feather="chevron-right" style="color: #5B6998"></i>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Modal -->
+        <div id="myModal" class="modal">
+            <div class="modal-content">
+                <div class="modal_header">
+                    <span>Ý kiến đóng góp mới</span>
+                </div>
+
+                <div class="modal_body">
+                    <input type="text" placeholder="Tiêu đề">
+                    <span>Nhập nội dung vào ô bên dưới</span>
+                    <textarea name="" id="" ></textarea>
+                </div>
+
+                <div class="modal_footer">
+                    <button type="button" class="btn_send">Gửi</button>
+                    <button type="button" class="close" onclick="closeModal()">Đóng</button>
+                </div>
+
+
+
+            </div>
+        </div>
+
     </main>
 
 
@@ -57,5 +114,22 @@
     feather.replace();
 </script>
 
+<script>
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+window.onclick = function(event) {
+    var modal = document.getElementById("myModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+</script>
 
 </html>
